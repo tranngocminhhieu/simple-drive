2e49dad35c0ff899df4b53fc27bf9608f29769b9
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2024-06-21 02:13:26 +0700

    Update drive.py

--- simple_drive/drive.py
+++ simple_drive/drive.py
@@ -687,11 +687,27 @@ class Drive:
             self.drive = drive
 
         def list(self, file_id):
+            '''
+            List all revisions
+            :param file_id: File ID
+            :return: List of revisions
+            '''
             return self.drive.service.revisions().list(fileId=file_id, fields='revisions').execute()['revisions']
 
         def get(self, file_id, revision_id):
+            '''
+            Get revision info
+            :param file_id: File ID
+            :param revision_id: Revision ID
+            :return: Revision info
+            '''
             return self.drive.service.revisions().get(fileId=file_id, revisionId=revision_id, fields='*').execute()
 
         def delete(self, file_id, revision_id):
+            '''
+            Delete a revision
+            :param file_id: File ID
+            :param revision_id: Revision ID
+            '''
             self.drive.service.revisions().delete(fileId=file_id, revisionId=revision_id).execute()
             self.drive.print_if_verbose(f"{Fore.RED}Deleted revision {Fore.RESET}{revision_id}")
