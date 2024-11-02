1d0c64a29caa910a2225b8836bc650db0235851a
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2024-11-02 16:17:57 +0700

    2.0.2

--- simple_drive/drive/permissions.py
+++ simple_drive/drive/permissions.py
@@ -7,19 +7,22 @@ class Permissions:
     def __init__(self, drive):
         self.drive = drive
 
-    def add(self, file_id, role, email=None, domain=None):
+    def add(self, file_id, role, email=None, domain=None, anyone=False):
         '''
         Add permission to a file or folder. Please provide exactly one of email or domain.
         :param file_id: File | folder ID.
         :param role: Use Roles or visit https://developers.google.com/drive/api/guides/ref-roles.
         :param email: Email address.
         :param domain: Domain, e.g. google.com.
+        :param anyone: Anyone with link.
         :return: Permission info.
         '''
-        provided_args = [email, domain]
-        provided_count = sum(arg is not None for arg in provided_args)
+
+
+        provided_args = [email, domain, anyone]
+        provided_count = sum([1 for arg in provided_args if arg is not None and arg is not False])
         if provided_count != 1:
-            raise ValueError("Please provide exactly one of email or domain.")
+            raise ValueError("Please provide exactly one of email, domain or anyone.")
 
         if isinstance(role, Enum):
             role_value = role.value
@@ -28,7 +31,9 @@ class Permissions:
             role_value = role.lower()
             role_name = role.capitalize()
 
-        if email:
+        if anyone:
+            body = {"type": "anyone", "role": role_value}
+        elif email:
             body = {"type": "user", "role": role_value, "emailAddress": email}
         elif domain:
             body = {"type": "domain", "role": role_value, "domain": domain}
@@ -47,28 +52,34 @@ class Permissions:
         :return: Ownership info.
         '''
         body = {'type': 'user', 'role': 'owner', 'emailAddress': email}
-        result = self.drive.service.permissions().create(fileId=file_id, body=body, transferOwnership=True,
-                                                         fields='*').execute()
+        result = self.drive.service.permissions().create(fileId=file_id, body=body, transferOwnership=True, fields='*').execute()
 
         self.drive.print_if_verbose(f"{Fore.BLUE}Transferred ownership of {Fore.RESET}{file_id} {Fore.BLUE}to {Fore.RESET}{email}")
 
         return result
 
-    def get(self, file_id, permission_id=None, email=None, domain=None):
+    def get(self, file_id, permission_id=None, email=None, domain=None, anyone=False):
         '''
         Get permission info. Please provide exactly one of permission_id, email, or domain.
         :param file_id: File | Folder ID.
         :param permission_id: Permission ID.
+        :param email: Email address.
+        :param domain: Domain, e.g. google.com.
+        :param anyone: Anyone with link.
         :return: Permission info.
         '''
-        provided_args = [permission_id, email, domain]
-        provided_count = sum(arg is not None for arg in provided_args)
+        provided_args = [permission_id, email, domain, anyone]
+        provided_count = sum([1 for arg in provided_args if arg is not None and arg is not False])
         if provided_count != 1:
-            raise ValueError("Please provide exactly one of permission_id, email, or domain.")
+            raise ValueError("Please provide exactly one of permission_id, email, domain or anyone.")
+
+
+        if anyone:
+            permission_id = 'anyoneWithLink'
 
         if permission_id:
-            return self.drive.service.permissions().get(fileId=file_id, permissionId=permission_id,
-                                                        fields='*').execute()
+            return self.drive.service.permissions().get(fileId=file_id, permissionId=permission_id, fields='*').execute()
+
         elif email or domain:
             permissions = self.list(file_id=file_id)
 
@@ -82,7 +93,7 @@ class Permissions:
             else:
                 raise ValueError(f"Permission not found: {email or domain}")
 
-    def update(self, file_id, role, permission_id=None, email=None, domain=None):
+    def update(self, file_id, role, permission_id=None, email=None, domain=None, anyone=False):
         '''
         Update a permission. Please provide exactly one of permission_id, email, or domain.
         :param file_id: File ID.
@@ -90,12 +101,13 @@ class Permissions:
         :param permission_id: Permission ID.
         :param email: Email address.
         :param domain: Domain, e.g. google.com.
+        :param anyone: Anyone with link.
         :return: Permission info.
         '''
-        provided_args = [permission_id, email, domain]
-        provided_count = sum(arg is not None for arg in provided_args)
+        provided_args = [permission_id, email, domain, anyone]
+        provided_count = sum([1 for arg in provided_args if arg is not None and arg is not False])
         if provided_count != 1:
-            raise ValueError("Please provide exactly one of permission_id, email, or domain")
+            raise ValueError("Please provide exactly one of permission_id, email, domain, or anyone")
 
         if isinstance(role, Enum):
             role_value = role.value
@@ -108,6 +120,8 @@ class Permissions:
 
         if permission_id:
             pass
+        elif anyone:
+            permission_id = 'anyoneWithLink'
         elif email or domain:
             permissions = self.list(file_id=file_id)
 
@@ -136,20 +150,24 @@ class Permissions:
         '''
         return self.drive.service.permissions().list(fileId=file_id, fields='permissions').execute()['permissions']
 
-    def remove(self, file_id, permission_id=None, email=None, domain=None):
+    def remove(self, file_id, permission_id=None, email=None, domain=None, anyone=False):
         '''
         Remove a permission from a file | folder. Please provide exactly one of permission_id, email, or domain.
         :param file_id: File | folder ID.
-        :param email: Email address.
         :param permission_id: Permission ID.
+        :param email: Email address.
+        :param domain: Domain, e.g. google.com.
+        :param anyone: Anyone with link.
         '''
-        provided_args = [permission_id, email, domain]
-        provided_count = sum(arg is not None for arg in provided_args)
+        provided_args = [permission_id, email, domain, anyone]
+        provided_count = sum([1 for arg in provided_args if arg is not None and arg is not False])
         if provided_count != 1:
-            raise ValueError("Please provide exactly one of permission_id, email, or domain.")
+            raise ValueError("Please provide exactly one of permission_id, email, domain, or anyone.")
 
         if permission_id:
             pass
+        elif anyone:
+            permission_id = 'anyoneWithLink'
         elif email or domain:
             permissions = self.list(file_id=file_id)
 
